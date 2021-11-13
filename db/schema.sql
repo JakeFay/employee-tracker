c4e3d4f41b9d3007d16a0fcfbe66688b491a32c9
Jake <jake.fay9673@gmail.com>
2021-11-13 12:33:45 -0500

    added tables and seeds

--- db/schema.sql
+++ db/schema.sql
@@ -1,20 +1,30 @@
-CREATE TABLE employee (
-    id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
-    role_id INTEGER FOREIGN KEY,
-    manager_id INTEGER FOREIGN KEY
+DROP DATABASE IF EXISTS employees;
+CREATE DATABASE employees;
+
+USE employees;
+
+CREATE TABLE department ( 
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) UNIQUE NOT NULL
 );
 
 CREATE TABLE role (
-    id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(30),
-    salary DECIMAL,
-    department_id INTEGER FOREIGN KEY
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) UNIQUE NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL, 
+    FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
-CREATE TABLE department ( 
-    id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30) NOT NULL
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    manager_id INT, 
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
+
+
 
--- db/seeds.sql
+++ db/seeds.sql
@@ -4,3 +4,28 @@ VALUES
     ('Engineering'),
     ('Finance'),
     ('Legal');
+
+
+INSERT INTO role (title, salary, department_id)
+VALUES
+('Sales Manager', 100000, 1),
+('Sales Person', 80000, 1),
+('Senior Dev', 175000, 2),
+('Jr Dev', 125000, 2),
+('Finance Manger', 100000, 3),
+('Underwriter', 75000, 3),
+('Lawyer', 200000, 4),
+('Legal Assistant', 85000, 4)
+;
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+    ('John', 'Doe', 1, NULL),
+    ('Jane', 'Smith', 2, 1),
+    ('Jack', 'Jackson', 3,NULL),
+    ('June', 'July', 4, 3),
+    ('Jerry', 'Rice', 7,NULL),
+    ('Janet', 'Early', 6, NULL)
+
+;
+
