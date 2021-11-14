ad6a75161ef3ba9694ec07be41c59196d65eca06
Jake <jake.fay9673@gmail.com>
2021-11-13 23:15:42 -0500

    added views

--- db/connection.js
+++ db/connection.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'pacnw420',
+    password: '',
     database: 'employees'
 });
 
