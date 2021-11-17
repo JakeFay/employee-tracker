dac049debb35cdcca169d684d09f589035a76c9f
Jake <jake.fay9673@gmail.com>
2021-11-17 15:43:28 -0500

    making sure everything is tracked

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const { prompt } = require('inquirer');
 require('console.table');
 const db = require('./db');
-const inquirer = require('inquirer');
 const connection = require('./db/connection');
 const { getAllEmployees, getAllRoles } = require('./db');
 
@@ -16,53 +15,38 @@ function getQuestion() {
         type: 'list',
         name: 'option',
         message: 'What would you like to do?',
-        choices: ['view all departments', 'view all roles', 'view all employees', 'add a department', 'add a role', 'add an employee', 'update an employee role', 'Quit']
+        choices: ['view all departments', 'view all roles', 'view all employees', 'add a department', 'add a role', 'add an employee', 'update employee', 'Quit']
     }]).then((res) => {
 
         switch (res.option) {
             case 'view all departments':
                 viewAllDepartments();
-                 break;
+                break;
 
             case 'view all roles':
                 viewAllRoles();
-                 break;
+                break;
 
             case 'view all employees':
                 viewAllEmployees();
                 break;
 
             case 'add a department':
-                inquirer.prompt([{
+                prompt([{
                     type: 'input',
                     name: 'name',
                     message: 'Type the name of the department you would like to add',
                 }])
-                .then((data)=> {
-                    console.log(data)
-                    addDepartment(data);
-                })
+                    .then((data) => {
+                        console.log(data)
+                        addDepartment(data);
+                    })
                 break;
 
-            case 'update an employee role':
-                try{
-                //viewAllEmployees()
-                //viewAllRoles()
-                inquirer.prompt([{
-                    type: 'input',
-                    name: 'employeeId',
-                    message: 'What is the id of the employee you want to update?'
-                },{
-                    type: 'input',
-                    name: 'roleId',
-                    message: 'what is the new role Id for this employee?'
-                }
-            ]).then((data)=> {
-                updateRole(data)
-            })
-        }catch(err){
-            console.log(err)
-        }
+            case 'update employee':
+
+                update()
+
 
             default:
                 process.exit();
@@ -91,14 +75,35 @@ function viewAllEmployees() {
 
 function addDepartment(answerObject) {
     connection.query('insert into department set ?', answerObject)
-    .then(() => getQuestion());
+        .then(() => getQuestion());
 };
 
 function updateRole(data) {
-    connection.query('update employee.role_id set role_id = ? WHERE id = ?', [data.roleId, data.employeeId], (err, result)=>{
+    connection.query('update employee.role_id set role_id = ? WHERE id = ?', [data.roleId, data.employeeId], (err, result) => {
         if (err) console.log(err)
         console.log(result)
     })
 }
-
+async function update(){
+    let data = await db.getAllRoles().then(([data]) => {
+         return data
+     })
+     let other = await db.getAllEmployees().then(([data]) => {
+         return data
+     })
+     console.table(data)
+     console.table(other)
+ prompt([{
+     type: 'input',
+     name: 'employeeId',
+     message: 'What is the id of the employee you want to update?'
+ },{
+     type: 'input',
+     name: 'roleId',
+     message: 'what is the new role Id for this employee?'
+ }
+ ]).then((data)=> {
+ updateRole(data)
+ })
+ }
 start();
